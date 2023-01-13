a805ecdbaeeedbbf0a8acbceb31d15688daffdd1
Sets2 <sah76@mail.ru>
2023-01-13 11:09:30 +0300

    Add project files.

---
+++ Core/Domain/Channel.cs
@@ -0,0 +1,15 @@
+﻿namespace NewsParser.Core.Domain;
+
+public class Channel
+{
+    public Guid Id { get; set; }
+    //Заголовок сайта - источника
+    public string? Title { get; set; }
+    //Описание сайта - источника
+    public string? Desc { get; set; }
+    //Ссылка на сайт источника
+    public string? Link { get; set; }
+
+    public virtual ICollection<Items>? Items { get; set; }
+
+}
