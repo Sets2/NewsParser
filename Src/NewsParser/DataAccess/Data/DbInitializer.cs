786ee4dcadda39f86a8b90acc6a49c52c2ed080b
Sets2 <sah76@mail.ru>
2023-01-16 11:04:51 +0300

    docker-compose

--- Src/NewsParser/DataAccess/Data/DbInitializer.cs
+++ Src/NewsParser/DataAccess/Data/DbInitializer.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Text.Json;
 using NewsParser.Core.Domain;
 using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
 
 namespace DataAccess.Data
 {
@@ -24,46 +25,56 @@ namespace DataAccess.Data
             const string channelFile = "dbInitChannel.json";
             const string itemFile = "dbInitChannelItems.json";
 
-//            await _dataContext.Database.EnsureDeletedAsync();
+            await _dataContext.Database.EnsureDeletedAsync();
             if (await _dataContext.Database.EnsureCreatedAsync())
             {
                 var path = _configuration["DbInit"];
 
-                if ((path != null) & File.Exists(path+ channelFile) & File.Exists(path+itemFile))
+                var dir = Directory.GetCurrentDirectory();
+                _logger.LogInformation("Текущий каталог " + dir);
+                _logger.LogInformation("Существует каталог инициализации " + path + " :" + Directory.Exists(path));
+                if (path == null)
                 {
-                    try
-                    {
-                        string? jsonString = File.ReadAllText(path + channelFile);
-                        if (jsonString?.Length > 1)
-                        {
-                            var listChannel = JsonSerializer.Deserialize<List<Channel>>(jsonString);
-                            if (listChannel != null)
-                            {
-                                await _dataContext.Channel.AddRangeAsync(listChannel);
-                                await _dataContext.SaveChangesAsync();
-                    }
-                        }
-
-                        jsonString = File.ReadAllText(path+itemFile);
-                        if (jsonString?.Length > 1)
-                        {
-                            var listItem = JsonSerializer.Deserialize<List<Item>>(jsonString);
-                            if (listItem != null)
-                            {
-                                await _dataContext.Item.AddRangeAsync(listItem);
-                                await _dataContext.SaveChangesAsync();
-                            }
-                        }
-                        _logger.LogInformation("Проведена инициализация из json файлов");
-
-                    }
-                    catch (Exception e)
-                    {
-                        _logger.LogError(e,e.Message+" Ошибка преобразования и записи данных для инициализации из json файлов");
-                    }
+                    _logger.LogWarning(
+                        "Отсутствует параметер DbInit в appsetting.json. Инициализация не проведена");
+                    return;
                 }
-                else _logger.LogInformation(
-                    "Отсутствует параметер DbInitChannel в appsetting.json или отсутствует уканный в нем файл. Инициализация не проведена");
+                if (!File.Exists(path + channelFile) | !File.Exists(path + itemFile))
+                {
+                    _logger.LogWarning(
+                        "Отсутствуют файлы для инициализации. Инициализация не проведена");
+                    return;
+                }
+                try
+                {
+                    string? jsonString = File.ReadAllText(path + channelFile);
+                    if (jsonString?.Length > 1)
+                    {
+                        var listChannel = JsonSerializer.Deserialize<List<Channel>>(jsonString);
+                        if (listChannel != null)
+                        {
+                            await _dataContext.Channel.AddRangeAsync(listChannel);
+                            await _dataContext.SaveChangesAsync();
+                }
+                    }
+
+                    jsonString = File.ReadAllText(path+itemFile);
+                    if (jsonString?.Length > 1)
+                    {
+                        var listItem = JsonSerializer.Deserialize<List<Item>>(jsonString);
+                        if (listItem != null)
+                        {
+                            await _dataContext.Item.AddRangeAsync(listItem);
+                            await _dataContext.SaveChangesAsync();
+                        }
+                    }
+                    _logger.LogInformation("Проведена инициализация из json файлов");
+
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e,e.Message+" Ошибка преобразования и записи данных для инициализации из json файлов");
+                }
             }
         }
     }
